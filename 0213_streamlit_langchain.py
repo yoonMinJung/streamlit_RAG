76d0a828cfc4832875316beeed818e76bea2e4e2
yoonMinJung <159868377+yoonMinJung@users.noreply.github.com>
2024-02-13 14:34:01 +0900

    Add files via upload

--- 0213_streamlit_langchain.py
+++ 0213_streamlit_langchain.py
@@ -56,7 +56,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
 from langchain.document_loaders import UnstructuredPowerPointLoader
-from langchain_community.document_loaders import UnstructuredExcelLoader
+from langchain.document_loaders import UnstructuredExcelLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
